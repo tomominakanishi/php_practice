20779710362848caacf9577bc7ef8ec1dfd933f6
tomominakanishi <tomomin1030@gmail.com>
2020-06-11 21:05:48 +0900

    third commit

---
+++ homework03/kadai3.php
@@ -0,0 +1,12 @@
+
+<?php
+
+//$fruits に配列で好きなフルーツを5個代入し、foreach文で順番に出力してください。
+
+$fruits = array("apple","banana","orange","mango","kiwi");
+
+foreach($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+}
+
