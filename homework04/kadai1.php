c177909e66e2b2ad9211b17d29fa3ce5c7e49331
tomominakanishi <tomomin1030@gmail.com>
2020-06-16 07:43:14 +0900

    fixed-1

--- homework04/kadai1.php
+++ homework04/kadai1.php
@@ -1,11 +1,9 @@
 <?php
 //引数に数値を指定して実行すると、数値を2倍にして返す関数を作成してください
 
-function kakezan(){
-
-//valueにすきな数値を指定する。
-  $value = 100;
-  echo $value * 2;
+function kakezan($value){
+  $result = $value * 2;
+  return $result;
 }
-  echo kakezan();
+  echo kakezan(100);
  ?>
--- homework04/kadai4.php
+++ homework04/kadai4.php
@@ -1,15 +1,15 @@
 <?php
-//【応用】　次のプログラムは、配列の中で1番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
+$arr = array(1,8,7,9,2);
 
 function max_array($arr){
 // とりあえず配列の最初の要素を一番大きい値とする
-$max_number = $arr[0];
-foreach($arr as $a){
-//どうしたらいいかわからない・・・・
-}
-
+ $max_number = $arr[0];
+ foreach((array)$arr as $a){
+   if($max_number < $a){
+     $max_number = $a;
+   }
+ }
 return $max_number;
-}
-
+ }
+echo max_array($arr);
  ?>
-
